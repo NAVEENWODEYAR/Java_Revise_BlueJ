2f7bb30d50825fc512e635482236f8ff82046efa
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-13 17:53:11 +0530

    ge

---
+++ com/basics/DataTypes.java
@@ -0,0 +1,15 @@
+package com.basics;
+
+public class DataTypes
+{
+    public static void main(String args[])
+    {
+        // Datatypes are basic blocks of all the programming languages..,
+        int a = 50000;
+        byte b = 25;
+        short c = 2500;
+        long d = 465464654;
+        System.out.println("Integer data types.,");
+        System.out.println("Integer,"+a+" Byte,"+b+" Short"+c+" Long,"+d);
+    }
+}
---
+++ com/basics/DataTypes1.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+
+public class DataTypes1
+{
+    public static void main(String args[])
+    {
+        // floating point datatypes,
+        float a = 3.144546546f;
+        double d = 654654654;
+        System.out.println("Float,"+a+" Double"+d);
+    }
+}
